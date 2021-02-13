5ed10fb60c924027a15a8805f1a11860c5479312
aldo.vernando <aldo.vernando@gmail.com>
2021-02-13 18:38:43 +0700

    remove pods

--- Pods/Target Support Files/Pods-FoodCatalogueModule-FoodCatalogueModuleTests/Pods-FoodCatalogueModule-FoodCatalogueModuleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FoodCatalogueModule_FoodCatalogueModuleTests : NSObject
-@end
-@implementation PodsDummy_Pods_FoodCatalogueModule_FoodCatalogueModuleTests
-@end
--- Pods/Target Support Files/Pods-FoodCatalogueModule/Pods-FoodCatalogueModule-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FoodCatalogueModule : NSObject
-@end
-@implementation PodsDummy_Pods_FoodCatalogueModule
-@end
